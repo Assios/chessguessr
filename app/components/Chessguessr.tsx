67fe4032c2ca237c3153c3107bd43b42e9ac7c87
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-06-16 00:02:41 +0200

    Show modal on solved

--- app/components/Chessguessr.tsx
+++ app/components/Chessguessr.tsx
@@ -3,6 +3,7 @@ import useChessguessr from "../hooks/useChessguessr";
 import { Chessboard } from "react-chessboard";
 import { Grid } from "./Grid";
 import styled from "styled-components";
+import Modal from "./Modal";
 
 const ChessboardWrapper = styled.div`
   display: flex;
@@ -51,54 +52,69 @@ export const Chessguessr = ({ data }: any) => {
 
   const { white, black, wRating, bRating } = data;
 
-  return (
-    <Game>
-      <BoardWrapper>
-        <Players>
-          <p className="text-2xl mb-4">
-            {white} ({wRating}) – {black} ({bRating})
-          </p>
-        </Players>
+  const [showModal, setShowModal] = useState(false);
 
-        <ChessboardWrapper>
-          {position && (
-            <Chessboard
-              arePiecesDraggable={currentGuess.length < 5 && !correct}
-              position={position.fen()}
-              onPieceDrop={onDrop}
-              areArrowsAllowed={false}
-            />
-          )}
-        </ChessboardWrapper>
-        <Buttons>
-          <button
-            className="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75 mr-2"
-            onClick={takeback}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 inline mr-1"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+  useEffect(() => {
+    if (correct) {
+      setTimeout(function () {
+        setShowModal(true);
+      }, 1600);
+    } else {
+      setShowModal(false);
+    }
+  }, [correct]);
+
+  return (
+    <div>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <Game>
+        <BoardWrapper>
+          <Players>
+            <p className="text-2xl mb-4">
+              {white} ({wRating}) – {black} ({bRating})
+            </p>
+          </Players>
+
+          <ChessboardWrapper>
+            {position && (
+              <Chessboard
+                arePiecesDraggable={currentGuess.length < 5 && !correct}
+                position={position.fen()}
+                onPieceDrop={onDrop}
+                areArrowsAllowed={false}
+              />
+            )}
+          </ChessboardWrapper>
+          <Buttons>
+            <button
+              className="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75 mr-2"
+              onClick={takeback}
             >
-              <path d="M8.445 14.832A1 1 0 0010 14v-2.798l5.445 3.63A1 1 0 0017 14V6a1 1 0 00-1.555-.832L10 8.798V6a1 1 0 00-1.555-.832l-6 4a1 1 0 000 1.664l6 4z" />
-            </svg>
-            Undo last move
-          </button>
-          <button
-            className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 ml-2"
-            onClick={submitGuess}
-          >
-            Submit
-          </button>
-        </Buttons>
-      </BoardWrapper>
-      <Grid
-        currentGuess={currentGuess}
-        guesses={guesses}
-        turn={turn}
-        insufficientMoves={insufficientMoves}
-      />
-    </Game>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 inline mr-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M8.445 14.832A1 1 0 0010 14v-2.798l5.445 3.63A1 1 0 0017 14V6a1 1 0 00-1.555-.832L10 8.798V6a1 1 0 00-1.555-.832l-6 4a1 1 0 000 1.664l6 4z" />
+              </svg>
+              Undo last move
+            </button>
+            <button
+              className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 ml-2"
+              onClick={submitGuess}
+            >
+              Submit
+            </button>
+          </Buttons>
+        </BoardWrapper>
+        <Grid
+          currentGuess={currentGuess}
+          guesses={guesses}
+          turn={turn}
+          insufficientMoves={insufficientMoves}
+        />
+      </Game>
+    </div>
   );
 };
---
+++ app/components/Modal.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function Modal({ showModal, setShowModal }) {
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Congrats!</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Jaddajadda
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
