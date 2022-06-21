0d75caa31112093790ed3d409ad2e199bd05f2db
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-06-21 23:59:26 +0200

    Test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -42,6 +42,17 @@ export const loader: LoaderFunction = async () => {
       bRating: 2855,
       id: 3,
     },
+    {
+      date: "2022-06-22",
+      fen: "r5k1/pb1p2q1/1p4p1/n1p3Q1/4p1P1/2N1P3/PPP2r2/1K1R3R w - - 0 25",
+      solution: ["Rd6", "Qf7", "Rxg6+", "Kf8", "Rh8+"],
+      gameUrl: "https://lichess.org/510uJ3P3/white#48",
+      white: "DrNykterstein",
+      black: "nihalsarin2004",
+      wRating: 3022,
+      bRating: 2855,
+      id: 4,
+    },
   ];
 
   return games.find((game) => {
