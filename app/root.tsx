77510b223a3adca46fe6e640db1558d39c6abae0
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-06-21 23:53:40 +0200

    Fix title

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Chessguessr",
   viewport: "width=device-width,initial-scale=1",
 });
 
