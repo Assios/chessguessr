617bee5854631206651e8b1ab746b383f406de74
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-06-23 00:54:17 +0200

    Add name

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { SiLichess } from "react-icons/si";
 
 export const Navbar = ({ fixed }) => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-500 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <nav className="relative flex items-center justify-between px-2 py-3 bg-blue-500 mb-3">
+        <div className="container px-4 mx-auto flex items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
+            <a className="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
               Chessguessr
             </a>
             <button
@@ -18,13 +19,17 @@ export const Navbar = ({ fixed }) => {
               <i className="fas fa-bars"></i>
             </button>
           </div>
-          <div
-            className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
-          ></div>
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <li className="nav-item">
+              <a
+                href="https://lichess.org/Assios"
+                target="_blank"
+                className="px-3 py-2 text-xl flex items-center font-bold text-white"
+              >
+                <SiLichess /> <span className="ml-2">Assios</span>
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
