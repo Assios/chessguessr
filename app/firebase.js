cc47c3aceb02a5b47f647bd7bc90dc404614d163
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-06-28 20:28:01 +0200

    Fix deps

--- app/firebase.js
+++ app/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,5 +16,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-export default db;
+
+export const db = getFirestore(app);
