20b65eefd2a580c4602beb9239f19ce8fa8e7176
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-08-25 00:26:04 +0200

    Questions

--- app/routes/guess/index.tsx
+++ app/routes/guess/index.tsx
@@ -9,6 +9,19 @@ import { Chessboard } from "react-chessboard";
 import { getPositions } from "~/models/guessGames.server";
 import useCountdown from "~/hooks/useCountdown";
 
+const getArrayOfRandomNumbers = (nums, arrayLength) => {
+  const numbers = [...Array(nums).keys()];
+  numbers.sort(() => Math.random() - 0.5);
+  return numbers.slice(0, arrayLength);
+};
+
+const getArrayOfRandomNumbersExcluding = (nums, arrayLength, excludingNum) => {
+  const numbers = [...Array(nums).keys()];
+  const numsExcluded = numbers.filter((i) => i !== excludingNum);
+  numsExcluded.sort(() => Math.random() - 0.5);
+  return numsExcluded.slice(0, arrayLength);
+};
+
 export const meta: MetaFunction = () => ({
   title: "Chessguessr Guess the game",
 });
@@ -34,6 +47,7 @@ const index = () => {
   const gPositions = () => {
     return [
       {
+        id: 1,
         fen: "2R5/5pkp/4pp2/pp6/3bq3/PP1N2P1/3RPP1P/6K1 b - - 0 27",
         white: "Magnus Carlsen",
         black: "Ian Nepomniachtchi",
@@ -42,6 +56,7 @@ const index = () => {
         link: "https://lichess.org/study/Hk9Zzv6o",
       },
       {
+        id: 2,
         fen: "r1r1k3/pp1qR2p/5pp1/3p2N1/6Q1/8/PP3PPP/2R3K1 b - - 0 22",
         white: "Wilhelm Steinitz",
         black: "Curt von Bardeleben",
@@ -50,12 +65,14 @@ const index = () => {
         link: "https://lichess.org/study/Hk9Zzv6o",
       },
       {
+        id: 3,
         fen: "6k1/2q2B1p/b1Pp2p1/8/1p1b4/1P3Q2/2P2RPP/4r1BK b - - 0 31",
         white: "Anatoly Karpov",
         black: "Garry Kasparov",
         link: "https://lichess.org/study/Hk9Zzv6o",
       },
       {
+        id: 4,
         fen: "1qr3k1/5Npp/p1p3n1/1r6/2R2bQ1/1P5P/PB3PP1/3R2K1 b - - 0 26",
         white: "Paul Keres",
         black: "Jose Raul Capablanca",
@@ -64,6 +81,7 @@ const index = () => {
         link: "https://lichess.org/study/Hk9Zzv6o",
       },
       {
+        id: 5,
         fen: "r3r1k1/pp3pbp/1qp1b1p1/2B5/2BP4/Q1n2N2/P4PPP/3R1K1R w - - 4 18",
         white: "Donald Byrne",
         black: "Bobby Fischer",
@@ -72,6 +90,7 @@ const index = () => {
         link: "https://lichess.org/study/Hk9Zzv6o",
       },
       {
+        id: 6,
         fen: "2kr4/ppp1qpp1/2p5/2b2b2/2P1pPP1/1P2P3/PBQPB1p1/RN2K2R w Q - 0 16",
         white: "Bent Larsen",
         black: "Boris Spassky",
@@ -83,9 +102,25 @@ const index = () => {
   };
 
   const makeQuiz = (numQuestions: number) => {
-    let allQuesitons = gPositions();
+    let allQuesitons = [...gPositions()];
+    const questionIndices = getArrayOfRandomNumbers(
+      allQuesitons.length,
+      numQuestions
+    );
 
     let newQuestions = [];
+
+    for (let i = 0; i < questionIndices.length; i++) {
+      const currentQuestionIndex = questionIndices[i];
+
+      const optionsIndices = getArrayOfRandomNumbersExcluding(
+        allQuesitons.length,
+        3,
+        currentQuestionIndex
+      );
+
+      // Add questions and options
+    }
 
     return allQuesitons;
   };
@@ -108,8 +143,6 @@ const index = () => {
   }, []);
 
   const q = makeQuiz(3);
-
-  console.log("q", q);
 
   return (
     <div>
