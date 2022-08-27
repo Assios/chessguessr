a48ed6a236401e1e98d53bb69971f277abf2ac35
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-08-27 15:03:11 +0200

    Add share link

--- app/components/Chessguessr.tsx
+++ app/components/Chessguessr.tsx
@@ -199,12 +199,39 @@ export const Chessguessr = ({
             </button>
           </Buttons>
         </BoardWrapper>
-        <Grid
-          currentGuess={currentGuess}
-          guesses={guesses}
-          turn={turn}
-          insufficientMoves={insufficientMoves}
-        />
+        <div className="flex flex-col">
+          <Grid
+            currentGuess={currentGuess}
+            guesses={guesses}
+            turn={turn}
+            insufficientMoves={insufficientMoves}
+          />
+          <div className="flex flex-row justify-end">
+            <input
+              type="text"
+              className="input input-bordered input-sm w-full max-w-xs mt-2 mr-1 w-56"
+              readOnly={true}
+              value={"https://chessguessr.com/game/57"}
+              onFocus={(event) => event.target.select()}
+            />
+            <button className="btn btn-square btn-sm bg-primary border-primary mt-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
       </Game>
     </div>
   );
