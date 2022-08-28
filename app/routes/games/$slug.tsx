4297603be368f535b23dc032afdc979246585dcd
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-08-29 00:04:58 +0200

    Catch error

--- app/routes/games/$slug.tsx
+++ app/routes/games/$slug.tsx
@@ -1,10 +1,27 @@
 import { Chessguessr } from "../../components/Chessguessr";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData, useOutletContext } from "@remix-run/react";
+import { useCatch, useLoaderData, useOutletContext } from "@remix-run/react";
 import { getGames } from "~/models/game.server";
 import { redirect } from "@remix-run/node";
 import { useEffect } from "react";
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <div className="mt-10 mb-20 content-center lg:mb-0">
+      <div className="flex flex-row justify-center">
+        <h1 className="text-center text-4xl mb-8 font-semibold">
+          {caught.status}
+        </h1>
+      </div>
+      <p className="max-w-prose m-auto text-center text-lg">
+        {caught.statusText}
+      </p>
+    </div>
+  );
+}
 
 export const loader: LoaderFunction = async ({ params, request }) => {
   const url = new URL(request.url);
@@ -16,13 +33,19 @@ export const loader: LoaderFunction = async ({ params, request }) => {
   const currentGame = games[index];
 
   if (!currentGame) {
-    throw new Response("Not Found", { status: 404 });
+    throw new Response("Not Found", {
+      status: 404,
+      statusText: `Game with ID ${params.slug} could not be found. Are you sure this is the correct URL?`,
+    });
   }
 
   if (currentGame.date === d) {
     return redirect("/");
   } else if (currentGame.date > d && dates !== "all") {
-    throw new Response("Not Found", { status: 404 });
+    throw new Response("Not Found", {
+      status: 404,
+      statusText: `Game with ID ${params.slug} could not be found. Are you sure this is the correct URL?`,
+    });
   }
 
   return json({ game: games[index] });
