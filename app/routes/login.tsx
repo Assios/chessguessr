dd41e996983b34a7583c44e8e133429d15f71c97
Asbjørn Steinskog <asbjorn@steinskog.me>
2023-08-29 23:09:29 +0200

    Style login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -39,57 +39,102 @@ export default function Login() {
   };
 
   return (
-    <div className="flex justify-center items-center">
-      <div className="mt-4 w-full max-w-md p-8 m-4 shadow-md rounded">
-        <h2 className="text-2xl font-bold mb-5 text-center">Log in</h2>
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight">
+          Sign in to Chessguessr
+        </h2>
+      </div>
 
-        <div className="form-control w-full max-w-xs">
-          <label className="label" htmlFor="identifier">
-            <span className="label-text">Username or Email:</span>
-          </label>
-          <input
-            type="text"
-            className="input input-bordered input-primary w-full max-w-xs"
-            id="identifier"
-            value={identifier}
-            onChange={(e) => setIdentifier(e.target.value)}
-          />
-        </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="identifier"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username or Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="identifier"
+                  type="text"
+                  value={identifier}
+                  onChange={(e) => setIdentifier(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-        <div className="form-control w-full max-w-xs">
-          <label className="label" htmlFor="password">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            className="input input-bordered input-primary w-full max-w-xs"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-        <div className="mb-4">
-          <button
-            type="submit"
-            className="mt-6 btn btn-primary"
-            onClick={handleSubmit}
-          >
-            {loading ? "Logging in..." : "Log in"}
-          </button>
-        </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                {loading ? "Logging in..." : "Log in"}
+              </button>
+            </div>
+          </form>
 
-        {message && (
-          <div className="text-center font-semibold text-red-500 mb-4">
-            {message}
+          {message && (
+            <div className="text-center font-semibold text-red-500 mb-4">
+              {message}
+            </div>
+          )}
+
+          <div className="relative mt-10">
+            <div
+              className="absolute inset-0 flex items-center"
+              aria-hidden="true"
+            >
+              <div className="w-full border-t border-gray-200" />
+            </div>
+            <div className="relative flex justify-center text-sm font-medium leading-6">
+              <span className="bg-white px-6 text-gray-900">
+                Or continue with
+              </span>
+            </div>
           </div>
-        )}
 
-        <div className="text-center">
-          Don't have an account?{" "}
-          <a href="/signup" className="text-blue-500 underline">
-            Sign Up
-          </a>
+          <div className="mt-6 grid grid-cols-2 gap-4">
+            <button className="flex w-full items-center justify-center gap-3 rounded-md bg-[#1D9BF0] px-3 py-1.5 text-white">
+              <span className="text-sm font-semibold leading-6">Twitter</span>
+            </button>
+            <button className="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white">
+              <span className="text-sm font-semibold leading-6">GitHub</span>
+            </button>
+          </div>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Don't have an account?{" "}
+            <a
+              href="/signup"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Sign Up
+            </a>
+          </p>
         </div>
       </div>
     </div>
