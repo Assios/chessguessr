c71a696ef771d3fc75cec0d8fd39674813b0cc85
Asbjørn Steinskog <asbjorn@steinskog.me>
2023-09-07 22:44:08 +0200

    Swap letters for icons

--- app/components/Grid.tsx
+++ app/components/Grid.tsx
@@ -1,6 +1,14 @@
 import { Row } from "./Row";
 import styled from "styled-components";
 import { GridProps } from "../utils/types";
+import {
+  FaChessKing,
+  FaChessQueen,
+  FaChessBishop,
+  FaChessPawn,
+  FaChessRook,
+  FaChessKnight,
+} from "react-icons/fa";
 
 const StyledGrid = styled.div`
   margin-left: 1rem;
@@ -11,21 +19,88 @@ const StyledGrid = styled.div`
   }
 `;
 
+export const convertToIcon = (move: string) => {
+  if (!move) return "";
+
+  if (move[0].toLowerCase() === move[0] && /[a-h]/.test(move[0])) {
+    return (
+      <>
+        <FaChessPawn /> {move}
+      </>
+    );
+  }
+
+  switch (move[0]) {
+    case "K":
+      return (
+        <>
+          <FaChessKing /> {move.substr(1)}
+        </>
+      );
+    case "Q":
+      return (
+        <>
+          <FaChessQueen /> {move.substr(1)}
+        </>
+      );
+    case "B":
+      return (
+        <>
+          <FaChessBishop /> {move.substr(1)}
+        </>
+      );
+    case "N":
+      return (
+        <>
+          <FaChessKnight /> {move.substr(1)}
+        </>
+      );
+    case "R":
+      return (
+        <>
+          <FaChessRook /> {move.substr(1)}
+        </>
+      );
+    default:
+      return move;
+  }
+};
+
 export const Grid = ({
   currentGuess,
   guesses,
   turn,
   insufficientMoves,
 }: GridProps) => {
+  const iconifiedCurrentGuess = currentGuess.map((move) => {
+    if (typeof move === "string") {
+      return convertToIcon(move);
+    }
+    return move;
+  });
+
+  const iconifiedGuesses = guesses.map((row) => {
+    return row.map((move: any) => {
+      if (move === null) {
+        return null;
+      }
+      return {
+        ...move,
+        move:
+          typeof move.move === "string" ? convertToIcon(move.move) : move.move,
+      };
+    });
+  });
+
   return (
     <StyledGrid>
       <p className="sm:text-lg lg:text-2xl mb-1 font-semibold">Your guesses:</p>
-      {guesses.map((guess: any, i: number) => {
+      {iconifiedGuesses.map((guess: any, i: number) => {
         if (turn === i) {
           return (
             <Row
               key={i}
-              currentGuess={currentGuess}
+              currentGuess={iconifiedCurrentGuess}
               guess={guess}
               insufficientMoves={insufficientMoves}
             />
