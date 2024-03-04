8fa4d58fd8cd8c9a13361a13a67c6a8474f00bc5
Asbjørn Steinskog <asbjorn@steinskog.me>
2024-03-04 22:04:18 +0100

    Remove slash

--- app/components/Chessguessr.tsx
+++ app/components/Chessguessr.tsx
@@ -166,7 +166,7 @@ export const Chessguessr = ({
       <Game>
         <div className="flex justify-center items-center w-full px-4 sm:px-0">
           <a
-            href="https://chessprophet.app/"
+            href="https://chessprophet.app"
             target="_blank"
             className="inline-block bg-gray-900 px-6 py-2.5 rounded-xl text-white text-sm mb-4 text-center"
             onClick={() => {
