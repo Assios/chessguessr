70fe045fe40dce19e8009879e251b73d3aaa39ea
Asbjørn Steinskog <asbjorn@steinskog.me>
2024-04-07 14:26:37 +0200

    Add banner

--- app/components/Chessguessr.tsx
+++ app/components/Chessguessr.tsx
@@ -164,6 +164,28 @@ export const Chessguessr = ({
         />
       </div>
       <Game>
+        <div className="flex justify-center items-center w-full px-4 sm:px-0">
+            <a
+              href="https://chessprophet.app"
+              target="_blank"
+              className="inline-block bg-gray-900 px-6 py-2.5 rounded-xl text-white text-sm mb-4 text-center border-2 border-white"
+              onClick={() => {
+                trackEvent("Click Chess Prophet");
+              }}
+            >
+              <strong>Sponsored</strong>
+              <svg
+                viewBox="0 0 2 2"
+                className="mx-2 inline h-0.5 w-0.5 fill-current"
+                aria-hidden="true"
+              >
+                <circle cx="1" cy="1" r="1" />
+              </svg>
+              New Chess Prophet game this tuesday! &nbsp;
+              <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+
         <BoardWrapper>
           <Players>
             <p className="sm:text-lg lg:text-2xl mb-2 font-semibold text-center">
