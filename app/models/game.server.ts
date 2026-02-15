c8a4d578a9c734c0248c17c7df8aaca3d5ff56f3
Asbjørn Steinskog <asbjorn@steinskog.me>
2026-02-15 15:52:34 +0100

    Add game

--- app/models/game.server.ts
+++ app/models/game.server.ts
@@ -17919,6 +17919,25 @@ export async function getGames(): Promise<Array<GameType>> {
       "wRating": 1554,
       "bRating": 1584,
       "id": 1335
+    },
+    {
+      "date": "2026-02-16",
+      "fen": "1r4k1/p4p1p/6p1/3rb3/K7/2PpB3/1P1R1PPP/3R4 b - - 2 25",
+      "solution": [
+        "Rd6",
+        "b4",
+        "Bxc3",
+        "Bc5",
+        "Ra6+"
+      ],
+      "gameUrl": "https://lichess.org/YRE2uOtG/black#49",
+      "white": "gogotaniye",
+      "black": "vizumatv4hoda",
+      "wTitle": "FM",
+      "bTitle": "GM",
+      "wRating": 2622,
+      "bRating": 2575,
+      "id": 1336
     }
   ];
 }
